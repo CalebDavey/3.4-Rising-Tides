e665c579e8ac7cfc68c0d3f26a434d5011b000f4
CalebDavey <caleb.yeedavey@gmail.com>
2020-09-22 09:44:38 +1200

    failed

--- Rising Tides/Assets/pillars.cs
+++ Rising Tides/Assets/pillars.cs
@@ -10,7 +10,6 @@ public class pillar1 : MonoBehaviour
 
     void Start()
     {
-        CreatePillar();
     }
 
     public void Update()
@@ -20,28 +19,4 @@ public class pillar1 : MonoBehaviour
             StartCoroutine("pillarRise");
         }
     }
-}
-
-public void CreatePillar()
-{
-    float offset = Random.Range(1, heightRandomisation);
-
-    // HEIGHTS DETERMINED BY PERLIN NOISE
-    height = Mathf.Round(Mathf.PerlinNoise((this.transform.position.x + offset) / 10, (this.transform.position.y + offset) / 10) * 10);
-}
-
-
-IEnumerator pillarRise()
-{
-    float StartTime = Time.time;
-    float EndTime = StartTime + time;
-
-    while (Time.time < EndTime)
-    {
-        float timeProgressed = (Time.time - StartTime) / time;  // this will be 0 at the beginning and 1 at the end.
-        this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, height, this.transform.position.z), timeProgressed);
-
-        yield return new WaitForFixedUpdate();
-    }
-}
 }
