0c1760e0583852b41606ef02330a85e0aaf13e2f
CalebDavey <caleb.yeedavey@gmail.com>
2020-09-22 09:59:13 +1200

    Pillar-Generation

---
+++ Rising Tides/Assets/Scripts/InitializePillars.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializePillars : MonoBehaviour
+{
+
+    public float gridSize = 5f;
+    public GameObject pillar;
+    public GameObject parent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       for(int x = 0; x < gridSize; x++)
+        {
+            for(int z = 0; z < gridSize; z++)
+            {
+                Instantiate(pillar, new Vector3(x, 0, z), Quaternion.identity, parent.transform);
+            }
+        }            
+    }
+}
