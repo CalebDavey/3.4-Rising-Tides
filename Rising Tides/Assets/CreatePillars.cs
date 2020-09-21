16ccc91a702666eb9f70ba9db2c6e2ae98c8f011
CalebDavey <caleb.yeedavey@gmail.com>
2020-09-22 09:41:55 +1200

    failed update
    This reverts commit 646eb2e788afddfc9d1c105f0ac9239955bc0da0.

---
+++ Rising Tides/Assets/CreatePillars.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePillars : MonoBehaviour
+{
+    public GameObject pillar;
+    public float gridSize = 5;
+
+    List<GameObject> pillarList = new List<GameObject>();
+    private void Start()
+    {
+        for (int x = 0; x < gridSize; x++)
+        {
+            for (int z = 0; z < gridSize; z++)
+            {
+                GameObject currentPillar = Instantiate(pillar, new Vector3(x, 0, z), Quaternion.identity);
+                pillarList.Add(currentPillar);
+            }
+        }
+    }
+    
+}
---
+++ Rising Tides/Assets/pillars.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pillar1 : MonoBehaviour
+{
+    float height = 5f;
+    public float time = 1f;
+    public float heightRandomisation = 5f;
+
+    void Start()
+    {
+        CreatePillar();
+    }
+
+    public void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine("pillarRise");
+        }
+    }
+}
+
+public void CreatePillar()
+{
+    float offset = Random.Range(1, heightRandomisation);
+
+    // HEIGHTS DETERMINED BY PERLIN NOISE
+    height = Mathf.Round(Mathf.PerlinNoise((this.transform.position.x + offset) / 10, (this.transform.position.y + offset) / 10) * 10);
+}
+
+
+IEnumerator pillarRise()
+{
+    float StartTime = Time.time;
+    float EndTime = StartTime + time;
+
+    while (Time.time < EndTime)
+    {
+        float timeProgressed = (Time.time - StartTime) / time;  // this will be 0 at the beginning and 1 at the end.
+        this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, height, this.transform.position.z), timeProgressed);
+
+        yield return new WaitForFixedUpdate();
+    }
+}
+}
