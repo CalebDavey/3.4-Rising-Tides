276e0559befe770368963f95854f817b945027ac
CalebDavey <caleb.yeedavey@gmail.com>
2020-10-04 14:49:37 +1300

    Movement Finished

--- Rising Tides/Assets/Scripts/playerMovement.cs
+++ Rising Tides/Assets/Scripts/playerMovement.cs
@@ -8,24 +8,41 @@ public class playerMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
     public Animator animator;
+    public Transform groundCheck;
+    public LayerMask groundMask;
 
     public float speed = 5;
     public float rotationSpeed = 0.15f;
     public float gravity;
     public float jumpHeight = 12;
+    public float checkDistance = 0.4f;
 
     float velocitySmoothing;
+    float ySpeed = 0;
 
+    bool isGrounded;
 
-    private void Update()
+    void Update()
     {
-        float ySpeed =+ gravity * Time.deltaTime;
+        move();
+        triggerAnimations();
+    }
+
+    void move()
+    {
         float horiz = Input.GetAxisRaw("Horizontal");
         float vert = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horiz, ySpeed, vert).normalized;
+        isGrounded = Physics.CheckSphere(groundCheck.position, checkDistance, groundMask);
 
-        if(direction.magnitude >= 0.1)
+        if (isGrounded && ySpeed < 0)
+        {
+            ySpeed = -gravity * Time.deltaTime;
+        }
+
+        Vector3 direction = new Vector3(horiz, 0, vert).normalized;
+
+        if (direction.magnitude >= 0.1)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref velocitySmoothing, rotationSpeed);
@@ -33,22 +50,38 @@ public class playerMovement : MonoBehaviour
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir * speed * Time.deltaTime);
-            
-            gravity -= 9.81f * Time.deltaTime;
-            controller.Move(new Vector3(0.0f, gravity, 0.0f));
-            if (controller.isGrounded)
-            {
-                gravity = 0;
-            }
         }
 
-        if(horiz != 0 || vert != 0)
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Debug.Log("Jump");
+            ySpeed = Mathf.Sqrt(jumpHeight * gravity);
+        }
+
+        ySpeed -= gravity * Time.deltaTime;
+        controller.Move(new Vector3(0.0f, ySpeed, 0.0f));
+    }
+
+    void triggerAnimations()
+    {
+        float horiz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+
+        if (isGrounded && horiz != 0 || vert != 0)
         {
             animator.SetBool("running", true);
-        } else
+            animator.SetBool("jumping", false);
+        }
+        if (ySpeed > 0)
         {
             animator.SetBool("running", false);
+            animator.SetBool("jumping", true);
         }
 
+        if(isGrounded && horiz == 0 && vert ==0)
+        {
+            animator.SetBool("running", false);
+            animator.SetBool("jumping", false);
+        }
     }
 }
